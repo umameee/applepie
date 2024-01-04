1e42a9aabaae850278cbeab31c6ac2740ff6288c
xing huang <miamiaxing123@gmail.com>
2024-01-03 22:14:29 -0500

    Initial Commit

---
+++ applepie/applepieApp.swift
@@ -0,0 +1,17 @@
+//
+//  applepieApp.swift
+//  applepie
+//
+//  Created by Xing huang on 1/3/24.
+//
+
+import SwiftUI
+
+@main
+struct applepieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
